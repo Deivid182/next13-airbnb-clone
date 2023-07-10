d69165083039ad1290ed7b95c6db62555560bb43
Dave <davidmendoza911119@gmail.com>
2023-07-09 19:02:00 -0600

    fixed error dynamic

--- app/components/ClientOnly.tsx
+++ app/components/ClientOnly.tsx
@@ -4,9 +4,22 @@ interface ClientOnlyProps {
   children: React.ReactNode
 }
 
+import { useEffect, useState } from "react"
+
 const ClientOnly: React.FC<ClientOnlyProps> = ({ children }) => {
+  
+  const [isMounted, setIsMounted] = useState(false)
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  if(!isMounted) return null
+
   return (
-    <div>{children}</div>
+    <>
+      {children}
+    </>
   )
 }
 
